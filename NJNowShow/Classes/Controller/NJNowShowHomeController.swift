c27af70dac1815beb3eff7e7787cfa6b4bcedc12
胡鹏 <64hp@163.com>
2018-07-09 20:07:43 +0800

    NJNowShowHomeController

---
+++ NJNowShow/Classes/Controller/NJNowShowHomeController.swift
@@ -0,0 +1,18 @@
+//
+//  NJNowShowHomeController.swift
+//  NJNowShow
+//
+//  Created by HuXuPeng on 2018/7/9.
+//
+
+import UIKit
+import NJKit
+
+class NJNowShowHomeController: NJViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
